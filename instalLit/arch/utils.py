6e730ad66bdf6f63e81559e3bc3b48ae666874fa
Bhavesh0327 <igj321182@gmail.com>
2021-05-02 00:01:05 +0530

    [Add] scraping functions

--- instalLit/arch/utils.py
+++ instalLit/arch/utils.py
@@ -1,5 +1,12 @@
 import os
 import sys
+import settings
+from bs4 import BeautifulSoup
+import requests
+import re
+from tabulate import tabulate
+
+
 
 def install(package):
     clone_command = f'git clone {package}'
@@ -22,18 +29,48 @@ def get_link(source):
     soup = BeautifulSoup(source.text , 'html.parser')
     return soup.find('table').find('a').text
 
-def main(package_name):
+def options(package_name):
     home_page = 'https://aur.archlinux.org/packages/'
-    source = requests.get(home_page).text
+    params = '?O=0&Seb=' + settings.search_by + '&K=' + package_name + '&outdated=' + settings.out_of_date + '&SB=' + settings.sort_by + '&SO=' + settings.sort_order + '&PP=' + str(settings.per_page)
+    req_page = home_page + params
+    source = requests.get(req_page)
+    soup = BeautifulSoup(source.text , 'html.parser')
+    data = soup.find('table', class_ = 'results')
+    if data == None:
+        return None
+    headings = []
+    params = data.find('thead')
+    if params:
+        params = params.find_all('th')
+    for param in params:
+        headings.append(param.text)
+    package_data = []
+
+    for each_package in data.find('tbody').find_all('tr'):
+        pack = list(filter(None, each_package.text.split('\n')))
+        package_data.append(pack)
+    
+    print("Please choose the name of package you want to install from this list\n")
+    
+    print(tabulate(package_data, headers=headings))
+    package_name = input("So what's the package you want to install now? ")
+    return package_name
+
+def package(package_name):
+    home_page = 'https://aur.archlinux.org/packages/'
 
     req_page = home_page + package_name
     source = requests.get(req_page)
     while(source.status_code == 404):
         print("The package you requested is not found, try again: ")
-        package_name = input()
+        package_name = options(package_name)
+        if package_name == None:
+            print("We couldn't find any package anyway related to the name you gave")
+            break
         req_page = home_page + package_name
         source = requests.get(req_page)
-    
-    print(get_link(source))
+    if source.status_code != 404:
+        print(get_link(source))
 
-install("https://aur.archlinux.org/brave.git")
+inp = input()
+package(inp)
